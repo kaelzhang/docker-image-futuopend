feb3b9898b51fdaf70ee545666cfd167ab091959
Kael Zhang <i+github@kael.me>
2023-08-29 21:28:28 +0800

    WIP

--- src/futu.js
+++ src/futu.js
@@ -18,30 +18,72 @@ module.exports = class FutuManager {
     api_port,
     server_port
   }) {
-    console.log('futu manager arguments', arguments)
-    this._status = STATUS.INIT
+    this._cmd = cmd
+    this._login_account = login_account
+    this._login_pwd_md5 = login_pwd_md5
+    this._login_region = login_region
+    this._lang = lang
+    this._log_level = log_level
+    this._api_port = api_port
 
     this._ws = new WebSocketServer({port: server_port})
+    this._clients = []
 
-    this._child = spawn(cmd, [
-      '-login_account', login_account,
-      '-login_pwd_md5', login_pwd_md5,
-      '-login_region', login_region,
-      '-lang', lang,
-      '-log_level', log_level,
-      '-api_port', api_port
-    ])
+    this._ws.on('connection', ws => {
+      this._clients.push(ws)
+      ws.on('error', err => console.error)
+      ws.on('message', msg => {
+        const payload = JSON.parse(msg)
+
+        if (payload.type === 'VERIFY_CODE') {
+          this.verify_code(payload.code)
+          return
+        }
+      })
+    })
+
+    this._init()
+  }
+
+  _init () {
+    this._status = STATUS.INIT
+
+    this._child = spawn(this._cmd, [
+      `-login_account=${this._login_account}`,
+      `-login_pwd_md5=${this._login_pwd_md5}`,
+      `-login_region=${this._login_region}`,
+      `-lang=${this._lang}`,
+      `-log_level=${this._log_level}`,
+      `-api_port=${this._api_port}`
+    ], {
+      // stdio: ['ignore', 'ignore', 'ignore']
+    })
 
     this._child.stdout.on('data', data => {
-      console.log('+++ data', typeof(data), data, data.toString())
+      console.log('[FutuOpenD] stdout:', data.toString())
+
+      if (data.toString().includes('req_phone_verify_code')) {
+        this._send({
+          type: 'REQUEST_VERIFY_CODE'
+        })
+        this._status = STATUS.REQUESTING_VERIFY_CODE
+        this._resolve()
+        return
+      }
     })
 
     this._child.stderr.on('data', (data) => {
-      console.error('+++ error', data, data.toString());
-    });
+      console.error('[FutuOpenD] process error:', data.toString())
+    })
 
     this._ready = new Promise((resolve, reject) => {
       this._resolve = resolve
+    })
+  }
+
+  _send (msg) {
+    this._clients.forEach(client => {
+      client.send(JSON.stringify(msg))
     })
   }
 
@@ -50,18 +92,26 @@ module.exports = class FutuManager {
 
     if (this._status === STATUS.REQUESTING_VERIFY_CODE) {
       this._set_verify_code(code)
+      return
+    }
+
+    if (this._status === STATUS.INIT) {
+      this._ready.then(() => {
+        this._set_verify_code(code)
+      })
     }
   }
 
   _set_verify_code (code) {
-    this._child.stdin.write(`input_phone_verify_code -code=${code}`)
+    this._child.stdin.write(`input_phone_verify_code -code=${code}\n`)
+    this._child.stdin.end()
   }
 
-  get is_ready () {
-    return this._status === STATUS.CONNECTED
-  }
+  // get is_ready () {
+  //   return this._status === STATUS.CONNECTED
+  // }
 
-  async ready () {
-    return this._ready
-  }
+  // async ready () {
+  //   return this._ready
+  // }
 }
--- start.js
+++ start.js
@@ -23,3 +23,22 @@ const futu = new FutuManager(FUTU_CMD, {
   api_port,
   server_port
 })
+
+// /usr/src/app/bin/FutuOpenD -login_account=621310 -login_pwd_md5=45313fc35a4f010d864d4bfca6582086 -login_region=sh -lang=en -log_level=debug -api_port=11112
+
+// const child = require('child_process').spawn('/usr/src/app/bin/FutuOpenD', [
+//   '-login_account', '621310',
+//   '-login_pwd_md5', '45313fc35a4f010d864d4bfca6582086',
+//   '-login_region', 'cn',
+//   '-lang', 'en',
+//   '-log_level', 'debug',
+//   '-api_port', '11111'
+// ])
+
+// child.stdout.on('data', data => {
+//   console.log('+++ data', typeof(data), data, data.toString())
+// })
+
+// child.stderr.on('data', (data) => {
+//   console.error('+++ error', data, data.toString())
+// })
